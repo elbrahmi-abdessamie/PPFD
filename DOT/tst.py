6a05dbde004c8f0bf638a0783bc679741bacec6a
bleuuone <bleuuone@gmail.com>
2024-08-17 13:20:36 +0100

    Data oriented

--- DOT/tst.py
+++ DOT/tst.py
@@ -1,45 +1,41 @@
 
 class Data:
     def __init__(self, args:list=None):
-        self.__index = self.__end = 0
         self.__args = args
-        if isinstance(args, list):
-            self.__end = len(args)
-    
-    def __iter__(self):
-        return self
 
-    def __next__(self):
-        if self.__args is None:
-            raise Exception("No data are provided")
-        if self.__index >= self.__end:
-            raise StopIteration
-        val = self.__args[self.__index]
-        self.__index += 1
-        return val
+    def get_data(self):
+        return self.__args
+
 
 class Methods:
     def __init__(self, args:dict=None):
-        self.__index = self.__end = 0
         self.__args = args
-        self.__keys = list(args.keys())
-        if isinstance(args, dict):
-            self.__end = len(self.__keys)
     
-    def __iter__(self):
-        return self
+    def get_methods(self):
+        return self.__args
 
-    def __next__(self):
-        if self.__args is None:
-            raise Exception("No data are provided")
-        if self.__index >= self.__end:
-            raise StopIteration
-        val = self.__args[self.__keys[self.__index]]
-        self.__index += 1
-        return val
+class StatisticTools:
 
-# for dt in Data([1,2,'3']):
+    @staticmethod
+    def mean(data: Data):
+        print(data.get_data())
+        pass
+
+    @staticmethod
+    def median(data: Data):
+        pass
+
+    @staticmethod
+    def quartile(data: Data):
+        pass
+
+    @staticmethod
+    def std(data: Data):
+        pass
+        
+
+StatisticTools.mean(Data([1,2,3]))
+
+
+# for dt in Methods({1:'lol', 2:'alo'}):
 #     print(dt)
-
-for dt in Methods({1:'lol', 2:'alo'}):
-    print(dt)
