53d8ed13b04535fd59e8bd663f6623a9454e0dfb
bleuuone <bleuuone@gmail.com>
2024-10-12 18:53:15 +0100

    feat: Refactor progress bar and color code in Loading.py

--- Starting/ex08/Loading.py
+++ Starting/ex08/Loading.py
@@ -1,18 +1,20 @@
 from os import get_terminal_size
 
 from time import sleep
-from tqdm import tqdm
+# from tqdm import tqdm
 
 def ft_tqdm(lst: range)-> list:
 
     total = len(lst)
     
     progress_bar_width = get_terminal_size().columns - 40
-
+    color_code = '\033[0;32m'
+    end_color = '\033[0m'
     for i in range(0, total + 1):
         progress = int(i / total * progress_bar_width)
-
-        progress_bar = f"|{'█' * progress:<{progress_bar_width}}|"
+        color_code = '\033[0;32m'
+        remain_space = f"{'▒' * (progress_bar_width - progress)}"
+        progress_bar = f" {color_code}{'█' * progress + end_color + '▒' * (progress_bar_width - progress)} "
         progress_percentage = progress * 100 // progress_bar_width
         progress_info = f"{progress_percentage}%{progress_bar} {i}/{total}"
 
@@ -24,8 +26,8 @@ def main():
 	for elem in ft_tqdm(range(333)):
 		sleep(0.005)
 	print()
-	for elem in tqdm(range(333)):
-		sleep(0.005)
+	# for elem in tqdm(range(333)):
+	# 	sleep(0.005)
 	print()
 
 if __name__ == "__main__":
--- Starting/ex08/test.py
+++ Starting/ex08/test.py
@@ -1,3 +1,15 @@
+from time import sleep
+from Loading import ft_tqdm
+
+for elem in ft_tqdm(range(333)):
+    sleep(0.005)
+print()
+
+
+# for elem in tqdm(range(333)):
+# sleep(0.005)
+# print()
+
 
 # def alo(string:str):
 #     return 'smash ' + string
